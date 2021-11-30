14dd915afa485306797705c0b5f9a2bacbb37a84
victortwu <victor607@gmail.com>
2021-11-29 21:23:12 -0800

    added some console logs

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -11,32 +11,38 @@ const latLong = [35.6762,139.6503]
 const geoString = '/locations/v1/cities/geoposition/search'
 const weatherString = '/currentconditions/v1/'
 
-// get geoposition AND weather in one call
+// get geoposition AND weather in one call, only 50 calls a day
 
-weather.get('/', async(req, res)=> { //needs params
+weather.get('/:geoCoord', async(req, res)=> { //needs params
     // needs to check for params
+    const coord = req.params.geoCoord
+    console.log(coord)
     try {
-        axios.get(`${accuUrl}${geoString}?apikey=${accuApiKey}&q=${latLong[0]},${latLong[1]}`)
+        axios.get(`${accuUrl}${geoString}?apikey=${accuApiKey}&q=${coord}`)
         .then(response=> {
-            //console.log('The Response: ',response)
+            console.log('Geo Response: ',response)
             return response.data
         })
         .then(data=> {
             axios.get(`${accuUrl}${weatherString}${data.Key}?apikey=${accuApiKey}`)
             .then(response=> {
+                console.log('Weather Response: ',response)
                 return response.data
             })
             .then(data=> {
                 res.status(200).json(data)
             })
             .catch(err=> {
+                console.log('Inner Catch')
                 console.error(err.message)
             })
         })
         .catch(err=> {
+            console.log('Outer Catch', err)
             console.error(err.message)
         })
     } catch(error) {
+        console.log('Global Catch')
         console.error(error.message)
     }
 })
